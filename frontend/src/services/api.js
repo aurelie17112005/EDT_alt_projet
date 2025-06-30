3178a0e7e342dc8267237ee129d9049b699f23cc
aurelie17112005 <denadi_aurelie.azonnoudo@edu.univ-fcomte.fr>
2025-06-30 11:04:26 +0100

    correction pour possibilité d'hébergement

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import store from '@/store';
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'http://localhost:3000',
-})
+    baseURL: process.env.VUE_APP_API_URL
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
