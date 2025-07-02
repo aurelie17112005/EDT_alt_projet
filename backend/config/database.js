98a0328023a3b58c3907ecc5cea81c73b0790ef7
aurelie17112005 <denadi_aurelie.azonnoudo@edu.univ-fcomte.fr>
2025-07-02 11:24:30 +0100

    correcting database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
-// Charger les variables d'environnement
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
-  port: process.env.DB_PORT,
-  logging: false, // ou true selon besoin
+  port: process.env.DB_PORT || 5432,
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
